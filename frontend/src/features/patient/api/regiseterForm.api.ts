8da772f888bf8bdc424afb88bb5b13928abe87b9
Annuanand2002 <168013538+Annuanand2002@users.noreply.github.com>
2026-09-23 13:37:27 +0530

    Debug patient API base URL

--- frontend/src/features/patient/api/regiseterForm.api.ts
+++ frontend/src/features/patient/api/regiseterForm.api.ts
@@ -9,6 +9,8 @@ export const createPatient = async (
   data: PatientRegistrationRequest,
 ): Promise<PatientRegistrationResponse> => {
   console.log("Startong....");
+  console.log("Startong....");
+  console.log("API BASE URL:", axiosInstance.defaults.baseURL);
   const response = await axiosInstance.post<PatientRegistrationResponse>(
     ROUTES.PATIENT.REGISTER.CREATE,
     data,
